9cfa4f81162061e2e5999a80c14723ecf9ea366e
losyash530 <mokinavi211t@gmail.com>
2024-10-24 22:25:40 +0300

    lesson 1 : !ProgramTasks

--- Лекция 1/Практические задания/!ProgramTasks/Program.cs
+++ Лекция 1/Практические задания/!ProgramTasks/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace A
+namespace H
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The first step!");
+            int baseNum = 42;
+            int stepen = 2;
+            int GetSquare = 1;
+
+            for (int i = 0; i < stepen; i++)
+            {
+                GetSquare *= baseNum;
+            }
+            Console.WriteLine(GetSquare);
         }
     }
 }
